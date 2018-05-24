37e110f5910da43ba8c6a12ff8b1c762712c0afa
haniev907 <35073345+haniev907@users.noreply.github.com>
2018-05-24 15:22:22 +0300

    start

---
+++ src/containers/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <div>Misha</div>
+    <div>Ibr</div>
+  </div>
+);
+
+export default App;
