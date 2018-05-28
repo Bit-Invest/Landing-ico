6e931f7e471e91aef47461437c6497f73b07db11
Dev <dev_1@crt.local>
2018-05-28 17:34:09 +0300

    Finish media block

--- src/components/common/MediaBlock/MediaBlock.js
+++ src/components/common/MediaBlock/MediaBlock.js
@@ -2,4 +2,15 @@ import React from 'react';
 
 const ROOT_CLASS = 'media-block';
 
-export const MediaBlock = props => <div className={ROOT_CLASS} />;
+export const MediaBlock = props => (
+  <div className={`${ROOT_CLASS}_active`}>
+    <img className={`${ROOT_CLASS}__picture`} src={props.imageSrc} />
+    <div className={`${ROOT_CLASS}__info`}>
+      <div className={`${ROOT_CLASS}__title`}>{props.title}</div>
+      <div className={`${ROOT_CLASS}__description`}>{props.description}</div>
+      <a href={props.href} className={`${ROOT_CLASS}__read-more`}>
+        read more
+      </a>
+    </div>
+  </div>
+);
