85cc13c60fe7593fe46fc5e29a4b33dc06c245ad
haniev907 <35073345+haniev907@users.noreply.github.com>
2018-06-03 18:52:48 +0300

    add icons soc

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import * as Scroll from 'react-scroll';
+import { socsSrc } from '@common/Links';
 
 const ROOT_CLASS = 'footer';
 
-const Socials = ['twitter', 'tumblr', 'linked-in', 'facebook'];
+const Socials = [
+  'medium',
+  'linkedin',
+  'facebook',
+  'twitter',
+  'reddit',
+  'telegram'
+];
 const scroll = Scroll.animateScroll;
 
 export const Footer = props => (
@@ -19,16 +27,18 @@ export const Footer = props => (
         let newSroll =
           Math.ceil((scrTop + 0.01) / global.HEIGHT) * global.HEIGHT;
 
-        console.log(scrTop, newSroll);
         scroll.scrollTo(newSroll);
       }}
     />
     <div className={`${ROOT_CLASS}__social-media`}>
       {Socials.map(social => (
-        <div
-          key={social}
-          className={`${ROOT_CLASS}__social ${ROOT_CLASS}__social_${social}`}
-        />
+        <a href={socsSrc[social][1]} target="__blank">
+          <img
+            src={socsSrc[social][0]}
+            key={social}
+            className={`${ROOT_CLASS}__social`}
+          />
+        </a>
       ))}
     </div>
   </div>
--- src/components/common/Links/index.js
+++ src/components/common/Links/index.js
@@ -1,3 +1,10 @@
+import medium_icon from '@assets/export_mountain_v2/icons/Medium.svg';
+import linkedin_icon from '@assets/export_mountain_v2/icons/IN.svg';
+import facebook_icon from '@assets/export_mountain_v2/icons/FB.svg';
+import twitter_icon from '@assets/export_mountain_v2/icons/Twitter.svg';
+import reddit_icon from '@assets/export_mountain_v2/icons/Reddit.svg';
+import telegram_icon from '@assets/export_mountain_v2/icons/telegram.svg';
+
 export const docs = {
   wp: 'https://drive.google.com/file/d/1QND8G_gccBJrZ1rBfh2Q8FE1iB-hiC-m/view',
   presentation:
@@ -6,4 +13,16 @@ export const docs = {
     'https://drive.google.com/file/d/1EQgDxF_vIVhW9gZ3Hn6DOnRwNg2x8h5X/view',
   math_model:
     'https://drive.google.com/file/d/1JBjf1PgKhm-d7Ft6Z7a60A9RFum_sw-h/view'
+};
+
+export const socsSrc = {
+  medium: [medium_icon, 'https://medium.com/cindx'],
+  linkedin: [
+    linkedin_icon,
+    'https://www.linkedin.com/company/cindex-platform/'
+  ],
+  facebook: [facebook_icon, 'https://www.facebook.com/cindx.io/'],
+  twitter: [twitter_icon, 'https://twitter.com/CindxPlatform'],
+  reddit: [reddit_icon, 'https://www.reddit.com/r/cindx/'],
+  telegram: [telegram_icon, 'https://t.me/cindx_official']
 };
