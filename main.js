560c5ea9ac22652f5832edd5b48b37176b045786
haniev907 <35073345+haniev907@users.noreply.github.com>
2018-07-09 18:56:53 +0300

    test without files #11

--- main.js
+++ main.js
@@ -635,47 +635,8 @@ var resizeFunc = (function() {
     $('body').css('overflow', 'auto');
   };
 
-  function linkScroll(elem) {
-    var sc_top = $(`[elem=${elem}]`).offset().top;
-    scrollBody(sc_top - 85, 1000);
-  };
-  window.popupVideo = function(src) {
-    $('popup[video] .th_iframe').html(`<iframe src="${src}" allow="autoplay; encrypted-media"></iframe>`);
-    //$('popup[video] iframe').attr('src', src);
-    $('popup[video]').show();
-  }
-  $('[anchor]').on('click', function(e) {
-    e.preventDefault();
-    var anchor = $(this).attr('anchor');
-    linkScroll(anchor)
-  })
-  $('popup[video] .close').on('click', function() {
-    $(this).closest('popup').hide()
-  })
-
   init();
   html_rr();
 });
+
 resizeFunc();
-//window.addEventListener("resize", resizeFunc, false);
-window.addEventListener("resize", function() {
-  clearTimeout(window.resizedFinished);
-  window.resizedFinished = setTimeout(function() {
-    console.log('Resized finished.');
-    resizeFunc();
-  }, 250);
-}, false);
-
-/**/
-$(document).ready(function(){
-  var elem = $('[toLoad]')[0];
-
-  $.get(
-    './desktop/index.html',
-    function(data){
-      elem.innerHTML = data;
-    }
-  );
-
-  //$('[toLoad]').load('./desktop/index.html');
-});
