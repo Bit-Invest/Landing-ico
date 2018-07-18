2f3ce7c457457f0237ab37ba9950f1255a9de653
MuradKurbanovDeveloper <mkurbanov92@mail.ru>
2018-07-18 19:56:05 +0300

    18.07 Murad

--- desktop/caruselinfo.js
+++ desktop/caruselinfo.js
@@ -73,6 +73,15 @@ define(function(){
       fackt3: 'Information Security focus',
       src: 'https://www.linkedin.com/in/ibrahimhaniev/'
     },
+    {
+      img: './media/Airat.jpg',
+      name: 'Airat Shaikhulov',
+      position: 'Head of Quantitative Research',
+      fackt1: 'Dr of science at MSU',
+      fackt2: 'Aton-line navigator developer',
+      fackt3: 'head of algorithmic trading',
+      src: 'https://www.linkedin.com/'
+    },
    ];
 
 
