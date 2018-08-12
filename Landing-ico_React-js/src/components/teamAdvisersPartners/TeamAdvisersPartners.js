50ef7a6b4636399a88b92e624381f8bbb9e7a8e3
Khaniev <haniev91@mail.ru>
2018-08-13 00:15:27 +0300

    fixed all blocks on all breakpoints

--- Landing-ico_React-js/src/components/teamAdvisersPartners/TeamAdvisersPartners.js
+++ Landing-ico_React-js/src/components/teamAdvisersPartners/TeamAdvisersPartners.js
@@ -40,165 +40,173 @@ export class TeamAdvisersPartners extends React.Component {
 
           <div className="team">
             <h2 className="header_blocks">Team of 40+ Members</h2>
-            <div className="settings">
-              <Slider {...settings}>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Igor Okatiev</h3>
-                  <p className="position">Chief Financial Officer</p>
-                  <hr />
-                  <p><span>5</span> years in entrepreneurship</p>
-                  <p><span>7</span> years in BRM</p>
-                  <p><span>10</span> years in financial markets</p>
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Yury Avdeev</h3>
-                  <p className="position">Chief Executive Officer</p>
-                  <hr />
-                  <p><span>13</span> years in entrepreneurship</p>
-                  <p><span>9</span> ventures</p>
-                  <p><span>$2m</span> for crypto management </p>
-                  <a href>
-                    <div src="../images/in.png" className="in" />
-                  </a>
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Artur Shamalov</h3>
-                  <p className="position">Investment Director</p>
-                  <hr />
-                  <p><span>15</span> years in entrepreneurship</p>
-                  <p><span>5000</span> mixcart.ru clients</p>
-                  <p><span>$6.5 bn</span> investor in dc-daily.ru</p><a href="#" className="in" />
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Denis Eskenazi</h3>
-                  <p className="position">Product Director</p>
-                  <hr />
-                  <p><span>7</span> years in entrepreneurship</p>
-                  <p><span>3</span> buisness degrees</p>
-                  <p><span>10</span> years in prezentation design</p><a href="#" className="in" />
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Igor Okatiev</h3>
-                  <p className="position">Chief Financial Officer</p>
-                  <hr />
-                  <p><span>5</span> years in entrepreneurship</p>
-                  <p><span>7</span> years in BRM</p>
-                  <p><span>10</span> years in financial markets</p><a href="#" className="in" />
-                </div>
-              </Slider>
+            <div className="block_arrow_relative">
+              <div className="settings">
+                <Slider {...settings}>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Igor Okatiev</h3>
+                    <p className="position">Chief Financial Officer</p>
+                    <hr />
+                    <p><span>5</span> years in entrepreneurship</p>
+                    <p><span>7</span> years in BRM</p>
+                    <p><span>10</span> years in financial markets</p>
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Yury Avdeev</h3>
+                    <p className="position">Chief Executive Officer</p>
+                    <hr />
+                    <p><span>13</span> years in entrepreneurship</p>
+                    <p><span>9</span> ventures</p>
+                    <p><span>$2m</span> for crypto management </p>
+                    <a href>
+                      <div src="../images/in.png" className="in" />
+                    </a>
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Artur Shamalov</h3>
+                    <p className="position">Investment Director</p>
+                    <hr />
+                    <p><span>15</span> years in entrepreneurship</p>
+                    <p><span>5000</span> mixcart.ru clients</p>
+                    <p><span>$6.5 bn</span> investor in dc-daily.ru</p><a href="#" className="in" />
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Denis Eskenazi</h3>
+                    <p className="position">Product Director</p>
+                    <hr />
+                    <p><span>7</span> years in entrepreneurship</p>
+                    <p><span>3</span> buisness degrees</p>
+                    <p><span>10</span> years in prezentation design</p><a href="#" className="in" />
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Igor Okatiev</h3>
+                    <p className="position">Chief Financial Officer</p>
+                    <hr />
+                    <p><span>5</span> years in entrepreneurship</p>
+                    <p><span>7</span> years in BRM</p>
+                    <p><span>10</span> years in financial markets</p><a href="#" className="in" />
+                  </div>
+                </Slider>
+              </div>
             </div>
-            <div className="settings2">
-              <Slider {...settingsTeam2}>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Igor Okatiev</h3>
-                  <p className="position">Chief Financial Officer</p>
-                  <hr />
-                  <p><span>5</span> years in entrepreneurship</p>
-                  <p><span>7</span> years in BRM</p>
-                  <p><span>10</span> years in financial markets</p>
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Yury Avdeev</h3>
-                  <p className="position">Chief Executive Officer</p>
-                  <hr />
-                  <p><span>13</span> years in entrepreneurship</p>
-                  <p><span>9</span> ventures</p>
-                  <p><span>$2m</span> for crypto management </p>
-                  <a href>
-                    <div src="../images/in.png" className="in" />
-                  </a>
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Artur Shamalov</h3>
-                  <p className="position">Investment Director</p>
-                  <hr />
-                  <p><span>15</span> years in entrepreneurship</p>
-                  <p><span>5000</span> mixcart.ru clients</p>
-                  <p><span>$6.5 bn</span> investor in dc-daily.ru</p><a href="#" className="in" />
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Denis Eskenazi</h3>
-                  <p className="position">Product Director</p>
-                  <hr />
-                  <p><span>7</span> years in entrepreneurship</p>
-                  <p><span>3</span> buisness degrees</p>
-                  <p><span>10</span> years in prezentation design</p><a href="#" className="in" />
-                </div>
-                <div className="item">
-                  <img src={ava_1} alt={"CINDX"}/>
-                  <h3>Igor Okatiev</h3>
-                  <p className="position">Chief Financial Officer</p>
-                  <hr />
-                  <p><span>5</span> years in entrepreneurship</p>
-                  <p><span>7</span> years in BRM</p>
-                  <p><span>10</span> years in financial markets</p><a href="#" className="in" />
-                </div>
-              </Slider>
+            <div className="block_arrow_relative">
+              <div className="settings2">
+                <Slider {...settingsTeam2}>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Igor Okatiev</h3>
+                    <p className="position">Chief Financial Officer</p>
+                    <hr />
+                    <p><span>5</span> years in entrepreneurship</p>
+                    <p><span>7</span> years in BRM</p>
+                    <p><span>10</span> years in financial markets</p>
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Yury Avdeev</h3>
+                    <p className="position">Chief Executive Officer</p>
+                    <hr />
+                    <p><span>13</span> years in entrepreneurship</p>
+                    <p><span>9</span> ventures</p>
+                    <p><span>$2m</span> for crypto management </p>
+                    <a href>
+                      <div src="../images/in.png" className="in" />
+                    </a>
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Artur Shamalov</h3>
+                    <p className="position">Investment Director</p>
+                    <hr />
+                    <p><span>15</span> years in entrepreneurship</p>
+                    <p><span>5000</span> mixcart.ru clients</p>
+                    <p><span>$6.5 bn</span> investor in dc-daily.ru</p><a href="#" className="in" />
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Denis Eskenazi</h3>
+                    <p className="position">Product Director</p>
+                    <hr />
+                    <p><span>7</span> years in entrepreneurship</p>
+                    <p><span>3</span> buisness degrees</p>
+                    <p><span>10</span> years in prezentation design</p><a href="#" className="in" />
+                  </div>
+                  <div className="item">
+                    <img src={ava_1} alt={"CINDX"}/>
+                    <h3>Igor Okatiev</h3>
+                    <p className="position">Chief Financial Officer</p>
+                    <hr />
+                    <p><span>5</span> years in entrepreneurship</p>
+                    <p><span>7</span> years in BRM</p>
+                    <p><span>10</span> years in financial markets</p><a href="#" className="in" />
+                  </div>
+                </Slider>
+              </div>
             </div>
           </div>
           <div className="clear" />
           <div className="advisers">
             <h2 className="header_blocks">Advisers</h2>
-            <Slider {...settingsAdvisers}>
-              <div>
-                <div className="left_content">
-                  <div className="icon_content"></div>
-                  <div className="name_content">Philip Staehelin</div>
-                  <hr/>
-                  <div className="text_content">
-                    <p>25 years of consulting and entrepreneurial experience</p>
-                    <p>$500m alternative payments business under leadership</p>
-                    <p>200+ startups from 20 countries mentored</p>
+            <div className="block_arrow_relative">
+              <div className="settings2_1">
+                <Slider {...settingsAdvisers}>
+                  <div>
+                    <div className="left_content">
+                      <div className="icon_content"></div>
+                      <div className="name_content">Philip Staehelin</div>
+                      <hr/>
+                      <div className="text_content">
+                        <p>25 years of consulting and entrepreneurial experience</p>
+                        <p>$500m alternative payments business under leadership</p>
+                        <p>200+ startups from 20 countries mentored</p>
+                      </div>
+                      <div className="ln"></div>
+                    </div>
+                    <div className="right_content">
+                      <p>
+                        Im an innovator and a leader who blends contagious optimism with pragmatic realism. I find opportunity where others see none.
+                        <br/><br/>
+                        Ive leveraged my ability to "see things differently" and "make things happen" in corporate, consulting and start-up environments - all giving me broad new perspectives that provide fodder for new transformative ideas.
+                        <br/><br/>
+                        Finding solutions to hard challenges is what gets me out of bed in the morning.
+                        <br/><br/>
+                        Career and projects: BCG, Spectacler, Transparency International, StartupYard, Bethreum, Gjirafa Inc, Direct People, CINDX
+                      </p>
+                    </div>
                   </div>
-                  <div className="ln"></div>
-                </div>
-                <div className="right_content">
-                  <p>
-                    Im an innovator and a leader who blends contagious optimism with pragmatic realism. I find opportunity where others see none.
-                    <br/><br/>
-                    Ive leveraged my ability to "see things differently" and "make things happen" in corporate, consulting and start-up environments - all giving me broad new perspectives that provide fodder for new transformative ideas.
-                    <br/><br/>
-                    Finding solutions to hard challenges is what gets me out of bed in the morning.
-                    <br/><br/>
-                    Career and projects: BCG, Spectacler, Transparency International, StartupYard, Bethreum, Gjirafa Inc, Direct People, CINDX
-                  </p>
-                </div>
-              </div>
 
-              <div>
-                <div className="left_content">
-                  <div className="icon_content"></div>
-                  <div className="name_content">Philip Staehelin</div>
-                  <hr/>
-                  <div className="text_content">
-                    <p>25 years of consulting and entrepreneurial experience</p>
-                    <p>$500m alternative payments business under leadership</p>
-                    <p>200+ startups from 20 countries mentored</p>
+                  <div>
+                    <div className="left_content">
+                      <div className="icon_content"></div>
+                      <div className="name_content">Philip Staehelin</div>
+                      <hr/>
+                      <div className="text_content">
+                        <p>25 years of consulting and entrepreneurial experience</p>
+                        <p>$500m alternative payments business under leadership</p>
+                        <p>200+ startups from 20 countries mentored</p>
+                      </div>
+                      <div className="ln"></div>
+                    </div>
+                    <div className="right_content">
+                      <p>
+                        Im an innovator and a leader who blends contagious optimism with pragmatic realism. I find opportunity where others see none.
+                        <br/><br/>
+                        Ive leveraged my ability to "see things differently" and "make things happen" in corporate, consulting and start-up environments - all giving me broad new perspectives that provide fodder for new transformative ideas.
+                        <br/><br/>
+                        Finding solutions to hard challenges is what gets me out of bed in the morning.
+                        <br/><br/>
+                        Career and projects: BCG, Spectacler, Transparency International, StartupYard, Bethreum, Gjirafa Inc, Direct People, CINDX
+                      </p>
+                    </div>
                   </div>
-                  <div className="ln"></div>
-                </div>
-                <div className="right_content">
-                  <p>
-                    Im an innovator and a leader who blends contagious optimism with pragmatic realism. I find opportunity where others see none.
-                    <br/><br/>
-                    Ive leveraged my ability to "see things differently" and "make things happen" in corporate, consulting and start-up environments - all giving me broad new perspectives that provide fodder for new transformative ideas.
-                    <br/><br/>
-                    Finding solutions to hard challenges is what gets me out of bed in the morning.
-                    <br/><br/>
-                    Career and projects: BCG, Spectacler, Transparency International, StartupYard, Bethreum, Gjirafa Inc, Direct People, CINDX
-                  </p>
-                </div>
+                </Slider>
               </div>
-            </Slider>
+            </div>
           </div>
           <div className="clear" />
           {/*<div className="ourPartners">
