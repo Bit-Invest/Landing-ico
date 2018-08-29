db5aa3c3c7726536219f65ab9bc0f069e41c50a4
MuradKurbanovDeveloper <mkurbanov92@mail.ru>
2018-08-29 16:35:33 +0300

    LANDING-34

--- Landing-ico_React-js/src/components/roadsShow/RoadsShow.js
+++ Landing-ico_React-js/src/components/roadsShow/RoadsShow.js
@@ -4,154 +4,274 @@ import './RoadsShow.css'
 
 const dataRoadShow = [
     {
-        place: 'San Francisco',
-        date: '1232-21',
-        data: '1',
+        place: 'Shanghai',
+        date: '30.08',
+        data: 'Borderless Blockchain Technology Summit',
         video: {
             img: '',
             src: ''
         },
-        text: '11'
+        text: '30 min ICO pitch'
     },
     {
-        place: 'San djasl',
-        date: '909-21',
-        data: '2',
+        place: 'Shanghai',
+        date: '7–8.09',
+        data: 'Slush',
         video: {
             img: '',
             src: ''
         },
-        text: '22'
+        text: 'Slush offers a unique opportunity to showcase your company to a global audience and to the who’s who in the tech community. Our only job is to help the next generation of world-conquering founders forward.'
     },
     {
-        place: 'Sadsakldmn Francisco',
-        date: '1232-21',
-        data: '3',
+        place: 'HONG KONG',
+        date: '9-10.09',
+        data: 'UNBLOCK COMMUNITY CONFERENCE',
         video: {
             img: '',
             src: ''
         },
-        text: '33'
+        text: 'Stand 20 min meet up with investors'
     },
     {
-        place: 'San Fracmxz,ncisco',
-        date: '3123-21',
-        data: '4',
+        place: 'Shanghai',
+        date: '11-12.09',
+        data: 'SHANGHAI INTERNATIONAL BLOCKCHAIN WEEK 2018 4th Global Blockchain Summit',
         video: {
             img: '',
             src: ''
         },
-        text: '44'
+        text: 'Vitalik Buterin'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '5',
+        place: 'Shanghai, Běijīng',
+        date: '9-16.09',
+        data: 'China Roadshow',
         video: {
             img: '',
             src: ''
         },
-        text: '55'
+        text: '1. Fee includes: All the transportation, meals, accommodation, all events and overseas assurance in China. 2. We will arrange translators during the whole tour. 3. Fee not includes round flights to China. 4. We will stay in 5 star hotels for double rooms, let us know if you want to upgrade for king size room. □Upgrade(Extra $160/person per day)'
     },
     {
-        place: 'San Fracmxz,ncisco',
-        date: '3123-21',
-        data: '6',
+        place: 'Singapore',
+        date: '14.09',
+        data: 'Slush',
         video: {
             img: '',
             src: ''
         },
-        text: '66'
+        text: 'At Slush Singapore 2018, 60 top-class startups will have an opportunity to showcase their businesses to investors, media and potential clients at the Demo Area. Being at the heart of the conference, Demo Stands are a fantastic platform to capture the interest of over 3,000 attendees.'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '7',
+        place: 'Seoul',
+        date: '16-19.09',
+        data: 'Blockseoul',
         video: {
             img: '',
             src: ''
         },
-        text: '77'
+        text: '"10min pitch, booth, 3 tickets stand"'
     },
     {
-        place: 'San Fracmxz,ncisco',
-        date: '3123-21',
-        data: '8',
+        place: 'Shanghai',
+        date: '18.09',
+        data: 'Blockchain Practitioner Conference China ',
         video: {
             img: '',
             src: ''
         },
-        text: '88'
+        text: '//Booth 2m*2m Panel speech 1/2 Ad 12000 Booth 2m*2m Keynote speech 1/2 Ad 14000'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '9',
+        place: 'Stockholm',
+        date: '19.09 ',
+        data: 'Blockchain & Bitcoin Conference',
         video: {
             img: '',
             src: ''
         },
-        text: '99'
+        text: '//'
     },
     {
-        place: 'San Fracmxz,ncisco',
-        date: '3123-21',
-        data: '10',
+        place: 'Singapore',
+        date: '19.09',
+        data: 'CONSENSUS Coindesk',
         video: {
             img: '',
             src: ''
         },
-        text: '1010'
+        text: '"SPONSORED NEWSLETTER @ $10,000 Take advantage of our branded newsletter by starting the narrative leading up to the Conference and reaching our 170,000+ subscriber base"'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '11',
+        place: 'Beijing China',
+        date: '19-20.09',
+        data: '5th Annual Block Chain Finance & Fin-tech China',
         video: {
             img: '',
             src: ''
         },
-        text: '1111'
+        text: '"Moderator of panel, 8 tickets, 2m*2m stand, 2 diner passes 2 tickets 2m*2m stand + 2 stuff tickets"'
     },
     {
-        place: 'San Fracmxz,ncisco',
-        date: '3123-21',
-        data: '12',
+        place: 'Moscow',
+        date: '25.09',
+        data: 'INTERNATIONAL BLOCKCHAIN SUMMIT',
         video: {
             img: '',
             src: ''
         },
-        text: '1212'
+        text: '"Stand 2meter+roll up + 2 tickets 1550 Stand 3 meter + roll up + 4 tickets VIP 2300"'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '13',
+        place: 'Banghok',
+        date: '25-26.09',
+        data: 'Digital economy. Think Asia',
         video: {
             img: '',
             src: ''
         },
-        text: '1313'
+        text: '"5 Minutes Stage Time 2 VIP Event Pass 2 B2B Networking Cruise Dinner Wristband 1 Networking Table"'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '14',
+        place: 'Kuala Lumpur',
+        date: '27-28.09',
+        data: 'Blockchain Project Showcases',
         video: {
             img: '',
             src: ''
         },
-        text: '1414'
+        text: '"in Malaysia Kuala lumpur at September 27,28 this year,you will see 100+ investors from VC/PE,Family office etc. Exhibition Sponsor: 8000USD Customized exhibit Space (3MX2M) One on one meeting with investors from VC,Capital Speech Sponsor:10000USD Keynote Speech (30 Minutes) One on one meeting with investors from VC,Capital"'
     },
     {
-        place: 'San Francisco',
-        date: '909-21',
-        data: '15',
+        place: 'Tokyo',
+        date: '28-29.09',
+        data: 'TEAMZ Blockchain Summit.',
         video: {
             img: '',
             src: ''
         },
-        text: '1515'
+        text: '"Investors, guest speakers kick-off party on Sep 27th (2 people/project) Booth (3m×2m) for Day 1 Lunch with investors, guest speakers for Day 1 (2 people/project) Summit pass for Day 1 & Day 2"'
+    },
+    {
+        place: 'Malta',
+        date: '03-05.10',
+        data: 'Delta Summit',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '"30 min Masterclass / Demo / Sponsor Presentation on MAIN STAGE  50 words company profile in event brochure  Social media post for Speaker + for company promo as official sponsor"'
+    },
+    {
+        place: 'London',
+        date: '9-10.10',
+        data: 'PayExpo',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '"As the UK’s largest payments event, PayExpo is essential for anyone involved and interested in making payments faster, easier and more secure. //Stand 6 aq m only 2 tickets"'
+    },
+    {
+        place: 'Shanghai',
+        date: '15-16.10',
+        data: 'IFPI2018: FinTech Future',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '// Lottery machine for investors cards'
+    },
+    {
+        place: 'Barselona',
+        date: '16-18.10',
+        data: 'ICO 2018',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '// special event for ICO investors'
+    },
+    {
+        place: 'Las Vegas',
+        date: '21-24.10',
+        data: 'Money20/20',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '"Money20/20 is the anchor event on the industry calendar where C-Level Executives, renowned speakers, innovators and disruptors from across the world unite to drive change in the future of money. // 70% of booths are booked."'
+    },
+    {
+        place: 'Dubai',
+        date: '24-25.10',
+        data: 'WORLD blockchain summit',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '"ICO Grand Slam is a great platform for those companies who are prepared to launch their ICO and the ones who are still planning on their ICO. They can pitch in ront of global investors including VCs, angel investors and key government authorities – and stand a chance to raise funds of upto US$ 2 million. Pitch only 6000 Pitch+table 10000"'
+    },
+    {
+        place: 'Beijing China',
+        date: '24-25.10',
+        data: 'BlockChain World Forum',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '//'
+    },
+    {
+        place: 'Austin USA',
+        date: '26-27.10',
+        data: 'Texas Bitcoin Conference 2018',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '//'
+    },
+    {
+        place: 'Malta',
+        date: '1-2.11',
+        data: 'MALTA BLOCKCHAIN SUMMIT',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '"includes 3 min Pitch on stage in front of savvy investors 2 metre booth (2*2 wall + table + 2 chairs) October 20 – Deadline for applications. October 21 – Top applicants will be invited to pitch. November 1-2 – ICO Pitch Competition (winners will be announced at the end of the conference). $100k PRIZE"'
+    },
+    {
+        place: 'Dubai',
+        date: '14-15.11',
+        data: 'Dubai Decentralized 2.0',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '10 min speech'
+    },
+    {
+        place: 'Moscow',
+        date: '20.11',
+        data: 'Blockchain Conference Russia',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '//'
+    },
+    {
+        place: 'Tokyo',
+        date: '8.12',
+        data: 'Blockchain & Criptocurrency',
+        video: {
+            img: '',
+            src: ''
+        },
+        text: '"ACCESS Makuhari Messe International Exhibition Hall 1 https://www.m-messe.co.jp/en/access/ Address: 2-1, Nakase, Mihama-ku, Chiba-city, 261-8550 Japan"'
     }
 ]
 
@@ -193,7 +313,7 @@ class RoadsShow extends Component {
         this.setState({
             valuePopUp: filterEl[0]
         })
-        
+
     }
 
     componentWillUnmount = () => {
