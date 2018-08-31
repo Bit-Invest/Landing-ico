209011e6c8dd338e381fa12fd764ee5a82d797c0
Alexey <lolokovalex@mail.ru>
2018-08-31 15:30:33 +0300

    Add langs and fixes.

--- Landing-ico_React-js/src/lngs/index.js
+++ Landing-ico_React-js/src/lngs/index.js
@@ -3,13 +3,21 @@ import ru from './ru';
 import zn from './zn';
 import ko from './ko'
 import tr from './tr'
+import fr from './fr'
+import ja from './ja'
+import pt from './pt'
+import vi from './vi'
 
 const indexLngObj = { 
     en,
     ru,
     zn,
     ko,
-    tr
+    tr,
+    fr,
+    ja,
+    pt,
+    vi
 }
 
 export default indexLngObj
