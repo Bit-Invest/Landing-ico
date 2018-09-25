5c61152b6924600d52c90377dfd74ee789d39004
Alexey <lolokovalex@mail.ru>
2018-09-25 17:52:40 +0300

    Add local storage ab_val.

--- Landing-ico_React-js/src/links.js
+++ Landing-ico_React-js/src/links.js
@@ -21,7 +21,9 @@ export const getGAID = function(){
 
 let clickid = linkSearch['clickid'];
 let gaid = getGAID;
-export const ab_val = linkSearch['ab_val']
+
+linkSearch['ab_val'] || window.localStorage.getItem('ab_val') ? window.localStorage.setItem('ab_val', 1) : null
+export const ab_val = window.localStorage.getItem('ab_val')
 
 export const list_lngs = 'en zn ko tr fr ja vi pt ar de es'; //языки который мы используем нужно тут указать
 const user_lng = window.localStorage.getItem('loc') || navigator.language || 'en';
