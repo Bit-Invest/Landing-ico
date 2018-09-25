4e4113851b85eb3f81137d853b0a8385c66620eb
Alexey <lolokovalex@mail.ru>
2018-09-25 18:43:44 +0300

    Add new.

--- Landing-ico_React-js/src/components/cryptoAssetsManagement/CryptoAssetsManagement.js
+++ Landing-ico_React-js/src/components/cryptoAssetsManagement/CryptoAssetsManagement.js
@@ -3,6 +3,9 @@ import Slider from 'react-slick'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { showPopUp, changeUrlPopupVideo } from '../../store/store';
+import crypto1 from '../../images/crypto/crypto1.png'
+import crypto2 from '../../images/crypto/crypto2.png'
+import crypto3 from '../../images/crypto/crypto3.png'
 import {links} from '../../links.js';
 // import play_button_youtube from '../../media/play_button_youtube.png';
 import './cryptoAssetsManagement.css';
@@ -31,38 +34,52 @@ const problemData = [
   }
 ]
 
+const cryptoArr = [
+  crypto1,
+  crypto2,
+  crypto3,
+]
+
 class CryptoAssetsManagement extends React.Component {
   state = {
-    isOpenDisc: true
+    isOpenDisc: true,
+    currentCrypto: 0
   }
 
   showVideoInPopUp = (url) => {
     this.props.changeUrlPopupVideo(url)
     this.props.showPopUp()
   }
+  
+  renderImgSlides = () => {
+    return cryptoArr.map((el, i) => {
+      return (
+        <div className={`problemBlock__slide problemBlock__slide-${i}`} key={i}>
+          <div style={{ backgroundImage: `url(${el})` }} className='problemBlock__slide-img'></div>
+        </div>
+      )
+    })
+  }
 
   renderProblemBlock = () => {
     return problemData.map((el, i) => {
       return (
         <div className="problemBlock__wrapper-problem" key={i}>
-        <div className="problemBlock">
-          <div className="txt">
-            <p className="caption">{ el.header }</p>
-            <hr/>
-            <p>
-              • { el.text1 }
-            </p>
-            <p>
-              • { el.text2 }
-            </p>
-            <p>
-              • { el.text3 }
-            </p>
+          <div className="problemBlock">
+            <div className="txt">
+              <p className="caption">{ el.header }</p>
+              <hr/>
+              <p>
+                • { el.text1 }
+              </p>
+              <p>
+                • { el.text2 }
+              </p>
+              <p>
+                • { el.text3 }
+              </p>
+            </div>
           </div>
-          <div className='video_block'>
-            <a target="_blank" href={links.mvp} className="btn">{ indexLngObj[lng]['cryptoAssetsManagement#6'] }</a>
-          </div>
-        </div>
         </div>
       )
     })
@@ -83,7 +100,7 @@ class CryptoAssetsManagement extends React.Component {
       fade: true,
     };
 
-    return(
+    return (
       <div name="product" id="product" className="cryptoAssetsManagement">
         <div className="size">
           <div className="row">
@@ -100,9 +117,19 @@ class CryptoAssetsManagement extends React.Component {
               />
             </div> */}
           </div>
-          <Slider { ...settings }>
-            { this.renderProblemBlock() }
-          </Slider>
+          <div className="problemBlock__wrapper">
+            <div className="problemBlock__slider">
+              <Slider { ...settings }>
+                { this.renderProblemBlock() }
+              </Slider>
+              <div className='video_block'>
+                <a target="_blank" href={links.mvp} className="btn">{ indexLngObj[lng]['cryptoAssetsManagement#6'] }</a>
+              </div>
+            </div>
+            <div className="problemBlock__img-slides">
+              { this.renderImgSlides() }
+            </div>
+          </div>
         </div>
         {/* <PopUpDisc /> */}
       </div>
