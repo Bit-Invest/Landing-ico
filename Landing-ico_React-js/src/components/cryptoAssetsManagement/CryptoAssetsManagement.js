2a7d6f6e389582e04b7e0e51f4e1e2451b291070
Alexey <lolokovalex@mail.ru>
2018-09-28 19:07:51 +0300

    Add new.

--- Landing-ico_React-js/src/components/cryptoAssetsManagement/CryptoAssetsManagement.js
+++ Landing-ico_React-js/src/components/cryptoAssetsManagement/CryptoAssetsManagement.js
@@ -3,9 +3,9 @@ import Slider from 'react-slick'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { showPopUp, changeUrlPopupVideo } from '../../store/store';
-import crypto1 from '../../images/crypto/crypto1.png'
-import crypto2 from '../../images/crypto/crypto2.png'
-import crypto3 from '../../images/crypto/crypto3.png'
+import crypto0 from '../../images/crypto/crypto1.png'
+import crypto1 from '../../images/crypto/crypto2.png'
+import crypto2 from '../../images/crypto/crypto3.png'
 import {links} from '../../links.js';
 // import play_button_youtube from '../../media/play_button_youtube.png';
 import './cryptoAssetsManagement.css';
@@ -19,14 +19,14 @@ const problemData = [
     text1: indexLngObj[lng]['cryptoAssetsManagement#3'],
     text2: indexLngObj[lng]['cryptoAssetsManagement#4'],
     text3: indexLngObj[lng]['cryptoAssetsManagement#5'],
-    mobileSrc: crypto1
+    mobileSrc: crypto0
   },
   {
     header: indexLngObj[lng]['cryptoAssetsManagement#7'],
     text1: indexLngObj[lng]['cryptoAssetsManagement#8'],
     text2: indexLngObj[lng]['cryptoAssetsManagement#9'],
     text3: indexLngObj[lng]['cryptoAssetsManagement#10'],
-    mobileSrc: crypto2
+    mobileSrc: crypto1
   },
   {
     header: indexLngObj[lng]['cryptoAssetsManagement#11'],
@@ -34,14 +34,14 @@ const problemData = [
     text2: indexLngObj[lng]['cryptoAssetsManagement#13'],
     text3: indexLngObj[lng]['cryptoAssetsManagement#14'],
     isMockup: true,
-    mobileSrc: crypto3
+    mobileSrc: crypto2
   }
 ]
 
 const cryptoArr = [
+  crypto0,
   crypto1,
-  crypto2,
-  crypto3
+  crypto2
 ]
 
 class CryptoAssetsManagement extends React.Component {
@@ -49,6 +49,9 @@ class CryptoAssetsManagement extends React.Component {
     isOpenDisc: true,
     currentCrypto: 0,
     isChange: false,
+    isDefault: true,
+    autoPlayInterval: null,
+    widthScreen: null
   }
 
   showVideoInPopUp = (url) => {
@@ -56,36 +59,77 @@ class CryptoAssetsManagement extends React.Component {
     this.props.showPopUp()
   }
 
-  // componentDidMount = () => {
-  //   setInterval(() => {
+  componentDidMount = () => {
+    window.addEventListener('resize', this._handlerResize)
+    this._handlerResize()
+  //   const autoPlayInterval = setInterval(() => {
   //     this.nextSlide()
-  //   }, 2000)
-  // }
+  //   }, 2500)
+
+  //   this.setState({ autoPlayInterval: autoPlayInterval })
+  }
 
   // componentWillUnmount = () => {
-  //   clearInterval()
+  //   clearInterval(this.state.autoPlayInterval)
   // }
+
+  _handlerResize = () => {
+    this.setState({
+      widthScreen: window.innerWidth
+    })
+  }
 
   renderImgSlides = () => {
     let currentCryptoArr = []
-    switch(this.state.currentCrypto) {
-      case 0:
-        currentCryptoArr = cryptoArr
-        break;
-      case 1:
-        currentCryptoArr = [ crypto2, crypto3, crypto1]
-        break;
-      case 2:
-        currentCryptoArr = [ crypto3, crypto1, crypto2]
-        break;
-      default:
-        currentCryptoArr = cryptoArr
-        break;
+    let svgBg = null
+
+    if (!this.state.isDefault) {
+      switch(this.state.currentCrypto) {
+        case 0:
+          currentCryptoArr = [ crypto2, crypto0, crypto1]
+          break;
+        case 1:
+          currentCryptoArr = [ crypto0, crypto1, crypto2]
+          break;
+        case 2:
+          currentCryptoArr = [ crypto1, crypto2, crypto0]
+          break;
+        default:
+          currentCryptoArr = cryptoArr
+          break;
+      }
+    } else {
+      currentCryptoArr = cryptoArr
+    }
+    
+    if (this.state.widthScreen > 1439) {
+      svgBg = (
+        <div className="problemBlock__more-block">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 573 435" preserveAspectRatio="none">
+            <polygon fill="rgba(255,255,255,0.5)" points="0,437 85,52 573,0 500,430"/>
+          </svg>
+          <div className="more-block__search-icon"></div>
+        </div>
+      )
+    } else if (this.state.widthScreen > 1023) {
+      svgBg = (
+        <div className="problemBlock__more-block">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 391 298" preserveAspectRatio="none">
+            <polygon fill="rgba(255,255,255,0.5)" points="0,298 85,52 391,0 391,298"/>
+          </svg>
+          <div className="more-block__search-icon"></div>
+        </div>
+      )
     }
     
     return currentCryptoArr.map((el, i) => {
+      const animation0 = this.state.isChange && i === 0 ? 'problemBlock__slide-animation-0' : ''
+      const animation1 = this.state.isChange && i === 1 ? 'problemBlock__slide-animation-1' : ''
+      const animation2 = this.state.isChange && i === 2 ? 'problemBlock__slide-animation-2' : ''
+
       return (
-        <div className={`problemBlock__slide problemBlock__slide-${i}`} key={i}>
+        <div className={`problemBlock__slide problemBlock__slide-${i} ${animation0} ${animation1} ${animation2}`} key={i}>
+          { i === 0 ? svgBg : null }
           <div style={{ backgroundImage: `url(${el})` }} className='problemBlock__slide-img'></div>
         </div>
       )
@@ -193,9 +237,16 @@ class CryptoAssetsManagement extends React.Component {
       dots: false,
       fade: true,
       adaptiveHeight: true,
+      beforeChange: () => {
+        this.setState({
+          isChange: true
+        })
+      },
       afterChange: (i) => {
         this.setState({
-          currentCrypto: i
+          currentCrypto: i,
+          isChange: false,
+          isDefault: false
         })
       }
     }
