fb28b69ba3b7fbbbdd7003cc705f9de742ec37df
hoano907 <haniev91@mail.ru>
2019-06-18 16:27:34 +0300

    push data urgent news

--- Landing_new/src/components/urgentNews/index.js
+++ Landing_new/src/components/urgentNews/index.js
@@ -6,11 +6,19 @@ import './style.css';
 let urgentNewsData = [
   {
     texts: {
-      title: 'Ibraghim Haniev CINDX Tech Lead',
-      readingTime: '3 MIN READ',
-      mainThought: 'Get to know more about the CINDX team!',
+      title: 'Initial Circulation Supply of CINXO',
+      readingTime: '4 MIN READ',
+      mainThought: 'Important information about CINDX tokens!', 
     },
-    linkMore: 'https://cindx.io/',
+    linkMore: 'https://medium.com/cindx/the-initial-circulating-supply-of-cinxo-tokens-386ae92b57cd',
+  },
+  {
+    texts: {
+      title: 'CINDX Token Benefits For Token Holders',
+      readingTime: '6 MIN READ',
+      mainThought: 'Get to know more about the CINDX tokens!',
+    },
+    linkMore: 'https://medium.com/cindx/cindx-token-benefits-for-token-holders-d3228205117c',
   },
 ];
 
@@ -22,7 +30,7 @@ class UrgentNest extends React.Component {
           <div className="childItem titleOne">{curUrgent.texts.title}</div>
           <div className="childItem readingTime">{curUrgent.texts.readingTime}</div>
           <div className="childItem mainThought">{curUrgent.texts.mainThought}</div>
-          <a className="readMore" href={curUrgent.linkMore}>Read More</a>
+          <a className="readMore" target="_blank" href={curUrgent.linkMore}>Read More</a>
         </div>
       ))
   
