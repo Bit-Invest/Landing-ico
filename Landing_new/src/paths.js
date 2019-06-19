8bfc7e44a2a3a307f5637ff52276ba596c02505c
hoano907 <haniev91@mail.ru>
2019-06-20 02:52:12 +0300

    optimization media images

--- Landing_new/src/paths.js
+++ Landing_new/src/paths.js
@@ -7,18 +7,18 @@ import Bogdan_Fiedur_feedback from './people/feedback/Bogdan_Fiedur.jpg';
 import Graham_Doggart_feedback from './people/feedback/Graham_Doggart.jpg';
 import Sadie_Hutton_feedback from './people/feedback/Sadie_Hutton.jpg';
 import Val_Jerdes_feedback from './people/feedback/Val_Jerdes.jpg';
-import Jason_King_feedback from './people/feedback/Jason_King.png';
+import Jason_King_feedback from './people/feedback/Jason_King.jpg';
 
 //team
 import Airat_team from './people/team/Airat.jpg';
-import Yuriy_Avdeev_team from './people/team/Yuriy_Avdeev.png';
+import Yuriy_Avdeev_team from './people/team/Yuriy_Avdeev.jpg';
 import Artur_Shamalov_team from './people/team/Artur_Shamalov.jpg';
 import Denis_Eskenazi_team from './people/team/Denis_Eskenazi.jpg';
-import Georg_Brich_team from './people/team/Georg_Brich.png';
+import Georg_Brich_team from './people/team/Georg_Brich.jpg';
 import Ibraghim_Haniev_team from './people/team/Ibraghim_Haniev.jpg';
-import Jelena_Karzetskaja_team from './people/team/Jelena_Karzetskaja.png';
+import Jelena_Karzetskaja_team from './people/team/Jelena_Karzetskaja.jpg';
 import Mofassair_Hossain_team from './people/team/Mofassair_Hossain.jpg';
-import Sergey_Potehin_team from './people/team/Sergey_Potehin.png';
+import Sergey_Potehin_team from './people/team/Sergey_Potehin.jpg';
 import Sofja_Pevzner_team from './people/team/Sofja_Pevzner.jpg';
 import Val_Jerdes_team from './people/team/Val_Jerdes.jpg';
 
@@ -27,15 +27,15 @@ import Amarpreet_Singh_advisers from './people/advisers/Amarpreet_Singh.jpg';
 import Austin_Kimm_advisers from './people/advisers/Austin_Kimm.jpg';
 import Bogdan_Fiedur_advisers from './people/advisers/Bogdan_Fiedur.jpg';
 import Graham_Doggart_advisers from './people/advisers/Graham_Doggart.jpg';
-import Jason_King_advisers from './people/advisers/Jason_King.png';
+import Jason_King_advisers from './people/advisers/Jason_King.jpg';
 import Keith_Teare_advisers from './people/advisers/Keith_Teare.jpg';
-import Kosol_Saisanit_advisers from './people/advisers/Kosol.png';
+import Kosol_Saisanit_advisers from './people/advisers/Kosol.jpg';
 import Leonard_Grayver_advisers from './people/advisers/Leonard_Grayver.png';
-import Lincoln_advisers from './people/advisers/Lincoln.png';
+import Lincoln_advisers from './people/advisers/Lincoln.jpg';
 import Philip_Staehelin_advisers from './people/advisers/Philip_Staehelin.jpg';
-import Reinhard_Berger_advisers from './people/advisers/Reinhard_Berger.png';
+import Reinhard_Berger_advisers from './people/advisers/Reinhard_Berger.jpg';
 import Sadie_Hutton_advisers from './people/advisers/Sadie_Hutton.jpg';
-import William_Zhu_advisers from './people/advisers/William_Zhu.png';
+import William_Zhu_advisers from './people/advisers/William_Zhu.jpg';
 
 export const peopleImages = {
 	feedback: {
