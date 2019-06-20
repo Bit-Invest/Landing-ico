99cd839de0b7f8ceb78407d38730d55226a48fab
hoano907 <haniev91@mail.ru>
2019-06-21 02:26:12 +0300

    {i18n}

--- CindxLanding/src/components/mailForm/mailForm.js
+++ CindxLanding/src/components/mailForm/mailForm.js
@@ -3,6 +3,9 @@ import { showPopUp, changeUrlPopupVideo } from '../../store/store';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import { lng, links } from '../../links.js';
+import indexLngObj from '../../lngs/index';
+
 import first_block_2_row_1 from '@images/first_block_2_row_1.png';
 import first_block_2_row_2 from '@images/first_block_2_row_2.png';
 import first_block_2_row_3 from '@images/first_block_2_row_3.png';
@@ -25,19 +28,19 @@ class MailForm extends React.Component {
           <a  target="__blank" href="http://static.cindx.io/en/crypto_currency_exchange_license.pdf">
             <div className={`${ROOT_CLASS}__item_legals`}>
               <img alt="" src={first_block_2_row_1} className="img_legal" />
-              <div className="text_legal">CRYPTOCURRENCY EXCHANGE LICENSE</div>
+              <div className="text_legal">{indexLngObj[lng]['teamLegal#3']}</div>
             </div>
           </a>
           <a  target="__blank" href="http://static.cindx.io/en/financial_institution_license.pdf">
             <div className={`${ROOT_CLASS}__item_legals`}>
               <img alt="" src={first_block_2_row_2} className="img_legal" />
-              <div className="text_legal">FINANCIAL INSTITUTION LICENSE</div>
+              <div className="text_legal">{indexLngObj[lng]['teamLegal#4']}</div>
             </div>
           </a>
           <a  target="__blank" href="http://static.cindx.io/en/crypto_wallet_license.pdf">
             <div className={`${ROOT_CLASS}__item_legals`}>
               <img alt="" src={first_block_2_row_3} className="img_legal" />
-              <div className="text_legal">CRYPTO WALLET LICENSE</div>
+              <div className="text_legal">{indexLngObj[lng]['teamLegal#5']}</div>
             </div>
           </a>
         </div>
